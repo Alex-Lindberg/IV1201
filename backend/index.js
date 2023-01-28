d28569e4762ae1a8e2010c483bed3e9157e655fc
Dennis Hadzialic <25294013+D3nnis38@users.noreply.github.com>
2023-01-28 12:28:40 +0100

    add basic logic for express server to init

---
+++ backend/index.js
@@ -0,0 +1,23 @@
+process.env.TZ = 'Europe/Stockholm';
+global.__basedir = __dirname;
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const routes = require('./routes');
+const app = express();
+const db = require('./utils/db');
+
+db.connect();
+
+const port = process.env.PORT || 3001;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+routes.init(app);
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}...`);
+  console.log('******* backend started *******');
+});
