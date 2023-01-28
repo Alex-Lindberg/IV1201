8dba965f3e1b0dd8cc701fb540dcd6cd891a7812
Dennis Hadzialic <25294013+D3nnis38@users.noreply.github.com>
2023-01-28 13:50:10 +0100

    update docker file

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -1,10 +1,10 @@
 const { Pool } = require('pg');
 const client = new Pool({
-  user: 'postgres',
-  password: 'postgres',
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
   host: 'localhost',
-  database: 'postgres',
-  port: 5432,
+  database: process.env.POSTGRES_DB || 'postgres',
+  port: process.env.POSTGRES_PORT || 5432,
 });
 
 const sendQuery = async (query) => {
@@ -16,6 +16,12 @@ const sendQuery = async (query) => {
   }
 };
 
-const connect = async () => await client.connect();
+const connect = async () => {
+  console.log('🚀 ~ file: db.js:9 ~ process.env.POSTGRES_USER', process.env.POSTGRES_USER);
+  console.log('🚀 ~ file: db.js:9 ~ process.env.POSTGRES_PASSWORD', process.env.POSTGRES_PASSWORD);
+  console.log('🚀 ~ file: db.js:9 ~ process.env.POSTGRES_DB', process.env.POSTGRES_DB);
+  console.log('🚀 ~ file: db.js:9 ~ process.env.POSTGRES_PORT', process.env.POSTGRES_PORT);
+  return await client.connect();
+};
 
 module.exports = { sendQuery, connect };
