31b44f8234b993eaf996815cf105db4341747dcf
Dennis Hadzialic <25294013+D3nnis38@users.noreply.github.com>
2023-01-28 12:58:53 +0100

    add openapi base-schema

---
+++ backend/api-documentation/index.js
@@ -0,0 +1,32 @@
+const parameters = require('./components/parameters');
+const schemas = require('./components/schemas');
+
+module.exports = {
+  openapi: '3.0.0',
+  info: {
+    version: '1.0',
+    title: 'IV1201 - API',
+    description: 'IV1201 - API',
+    contact: {
+      name: 'IV1201 - API',
+    },
+  },
+  servers: [
+    {
+      url: '/api',
+    },
+  ],
+  security: [{ XAccessTokenAuth: [] }],
+  paths: {},
+  components: {
+    securitySchemes: {
+      XAccessTokenAuth: {
+        type: 'apiKey',
+        in: 'header',
+        name: 'X-ACCESS-TOKEN',
+      },
+    },
+    parameters,
+    schemas,
+  },
+};
