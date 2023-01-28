1978d269995a85bc6720f173a60abd901d3154e2
Dennis Hadzialic <25294013+D3nnis38@users.noreply.github.com>
2023-01-28 12:59:14 +0100

    initialized openApi with swagger docs

---
+++ backend/routes.js
@@ -0,0 +1,45 @@
+const swaggerUi = require('swagger-ui-express');
+const openapi = require('express-openapi');
+const path = require('path');
+
+const createApiDocPaths = require('./utils/createApiDocPaths');
+
+function initOpenApiRoutes({ app, apiDoc, apiSrcPath }) {
+  openapi.initialize({
+    apiDoc: apiDoc,
+    app: app,
+    paths: path.resolve(__dirname, apiSrcPath),
+  });
+}
+
+function getJson(apiDoc) {
+  return (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(apiDoc);
+  };
+}
+
+function getHtml(apiDoc) {
+  const options = {
+    explorer: true,
+  };
+
+  return [swaggerUi.serveFiles(apiDoc, options), swaggerUi.setup(apiDoc)];
+}
+
+function init(app) {
+  const apiDoc = require('./api-documentation');
+
+  const apiDocPathsAppended = { ...apiDoc, paths: createApiDocPaths(path.resolve(__dirname, 'api-routes')) };
+  app.use('/api/api-docs.json', getJson(apiDocPathsAppended));
+  app.use('/api/api-docs', getHtml(apiDocPathsAppended));
+  initOpenApiRoutes({
+    app: app,
+    apiDoc: apiDoc,
+    apiSrcPath: './api-routes',
+  });
+}
+
+module.exports = {
+  init,
+};
