f19884450ddaa00551d096240f6747cb02e5ff20
Lucas Larsson <70958503+lucas-larsson@users.noreply.github.com>
2023-02-05 23:37:20 +0100

    removed redundant code

--- backend/utils/dbIntegration/dbConfig.js
+++ backend/utils/dbIntegration/dbConfig.js
@@ -1,23 +1,8 @@
 const { Pool } = require('pg');
-const fs = require('fs');
 
 const client = new Pool({
 	connectionString: process.env.DATABASE_URL,
 });
-const dbCreator = fs
-	.readFileSync('utils/dbIntegration/sqlScripts/old-database.sql')
-	.toString();
-
-const createDatabase = async () => {
-	try {
-		await client.query('BEGIN');
-		await client.query(dbCreator);
-		await client.query('COMMIT');
-	} catch (error) {
-		await client.query('ROLLBACK');
-		throw error;
-	}
-};
 
 const connect = async () => {
 	client.connect();
@@ -30,16 +15,5 @@ const sendQuery = async (query) => {
 		throw error;
 	}
 };
-
-// check if database "IV1012" exists, if it doesn't, create it
-sendQuery(`SELECT * FROM pg_database WHERE datname = 'iv1201'`)
-	.then((res) => {
-		if (res.rowCount === 0) {
-			createDatabase();
-		}
-	})
-	.catch((error) => {
-		console.log(error);
-	});
 
 module.exports = { sendQuery, connect };
