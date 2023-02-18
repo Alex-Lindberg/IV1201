3a7dc697d4ebbb41b6468f40ea7afd5b7c6d7391
Lucas Larsson <70958503+lucas-larsson@users.noreply.github.com>
2023-02-18 13:06:50 +0100

    updated project

--- backend/utils/dbIntegration/dbConfig.js
+++ backend/utils/dbIntegration/dbConfig.js
@@ -1,11 +1,7 @@
 const { Pool } = require('pg');
 
 const client = new Pool({
-	user: process.env.POSTGRES_USER,
-	host: process.env.POSTGRES_HOST,
-	database: process.env.POSTGRES_DB,
-	password: process.env.POSTGRES_PASSWORD,
-	port: process.env.POSTGRES_PORT,
+	connectionString: process.env.DATABASE_URL,
 });
 
 const connect = async () => {
