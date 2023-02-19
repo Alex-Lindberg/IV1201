81b296e2c56fd66f71067a5bcf8fd6898a6ce8ed
Lucas Larsson <70958503+lucas-larsson@users.noreply.github.com>
2023-02-19 14:25:57 +0100

    added docker file

--- backend/utils/dbIntegration/dbConfig.js
+++ backend/utils/dbIntegration/dbConfig.js
@@ -1,8 +1,28 @@
 const { Pool } = require('pg');
 
-const client = new Pool({
-	connectionString: process.env.DATABASE_URL,
-});
+let client;
+if (process.env.NODE_ENV !== 'development') {
+	console.log('process.env.DATABASE_URL', process.env.DATABASE_URL);
+	client = new Pool({
+		connectionString: process.env.DATABASE_URL,
+	});
+} else {
+	client = new Pool({
+		host: process.env.POSTGRES_HOST,
+		user: process.env.POSTGRES_USER,
+		password: process.env.POSTGRES_PASSWORD,
+		database: process.env.POSTGRES_DB,
+		socketPath: process.env.POSTGRES_SOCKET_PATH,
+		port: process.env.POSTGRES_PORT,
+	});
+}
+// temp logs to be removed later
+console.log('host', process.env.POSTGRES_HOST);
+console.log('user', process.env.POSTGRES_USER);
+console.log('password', process.env.POSTGRES_PASSWORD);
+console.log('database', process.env.POSTGRES_DB);
+console.log('socketPath', process.env.POSTGRES_SOCKET_PATH);
+console.log('port', process.env.POSTGRES_PORT);
 
 const connect = async () => {
 	client.connect();
