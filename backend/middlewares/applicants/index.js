b8a3d5b69103eb4a43b738e9083137ce83cd5c81
Dennis Hadzialic <25294013+D3nnis38@users.noreply.github.com>
2023-02-21 20:33:16 +0100

    add documented middlewares

--- backend/middlewares/applicants/index.js
+++ backend/middlewares/applicants/index.js
@@ -40,7 +40,91 @@ const getAllApplicants = async (req, res, next) => {
 	}
 };
 
+/**
+ * It gets an applicant from the database and adds it to the response object
+ * @param req - The request object
+ * @param res - The response object
+ * @param next - This is a function that you call when you're done with your middleware.
+ * @returns The applicant object is being returned.
+ */
+const getApplicant = async (req, res, next) => {
+	const applicantId = req.params.applicantId;
+	try {
+		res.locals.outData.applicant = await applicantsDAO.getApplicant(
+			applicantId
+		);
+		next();
+	} catch (err) {
+		console.error('Error in getApplicant: ', err.message);
+		return next(
+			errorCodes.serverError({
+				req,
+				message: 'Could not fetch applicant',
+			})
+		);
+	}
+};
+
+/**
+ * It fetches the availability for an applicant
+ * @param req - The request object
+ * @param res - The response object.
+ * @param next - This is a function that you call when you're done with your middleware.
+ * @returns An array of objects with the following properties:
+ */
+const getAvailabilityForApplicant = async (req, res, next) => {
+	const include = req.query.include;
+	if (!include.includes('availability')) {
+		return next();
+	}
+	const applicantId = req.params.applicantId;
+	try {
+		res.locals.outData.applicant.availability =
+			await applicantsDAO.getAvailabilityForApplicant(applicantId);
+		next();
+	} catch (err) {
+		console.error('Error in getAvailabilityForApplicant: ', err.message);
+		return next(
+			errorCodes.serverError({
+				req,
+				message: 'Could not fetch applicant availability',
+			})
+		);
+	}
+};
+
+/**
+ * It fetches the competence for an applicant
+ * @param req - The request object
+ * @param res - The response object.
+ * @param next - The next middleware function in the stack.
+ * @returns The applicant's competence.
+ */
+const getCompetenceForApplicant = async (req, res, next) => {
+	const include = req.query.include;
+	if (!include.includes('competence')) {
+		return next();
+	}
+	const applicantId = req.params.applicantId;
+	try {
+		res.locals.outData.applicant.competence =
+			await applicantsDAO.getCompetenceForApplicant(applicantId);
+		next();
+	} catch (err) {
+		console.error('Error in getCompetenceForApplicant: ', err.message);
+		return next(
+			errorCodes.serverError({
+				req,
+				message: 'Could not fetch applicant competence',
+			})
+		);
+	}
+};
+
 module.exports = {
 	initLocals,
 	getAllApplicants,
+	getApplicant,
+	getAvailabilityForApplicant,
+	getCompetenceForApplicant,
 };
