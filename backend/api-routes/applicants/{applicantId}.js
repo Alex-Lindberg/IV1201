d8e89861f466792151fd27f4cfc399429d72a5ca
Dennis Hadzialic <25294013+D3nnis38@users.noreply.github.com>
2023-02-21 20:33:38 +0100

    add GET-applicants/{id}-route

---
+++ backend/api-routes/applicants/{applicantId}.js
@@ -0,0 +1,36 @@
+const auth = require('../../middlewares/auth'),
+	applicantsMiddleware = require('../../middlewares/applicants'),
+	responseMiddleware = require('../../middlewares/response');
+
+module.exports = {
+	get: [
+		auth.initLocals,
+		auth.authorize,
+		applicantsMiddleware.initLocals,
+		applicantsMiddleware.getApplicant,
+		applicantsMiddleware.getAvailabilityForApplicant,
+		applicantsMiddleware.getCompetenceForApplicant,
+		responseMiddleware.sendResponse(200, 'applicant'),
+	],
+};
+
+module.exports.get.apiDoc = {
+	tags: ['applicants'],
+	responses: {
+		200: {
+			description: 'Successfully fecthed an applicant',
+			content: {
+				'application/json': {
+					schema: {
+						$ref: '#/components/schemas/Applicant',
+					},
+				},
+			},
+		},
+	},
+	parameters: [
+		{
+			$ref: '#/components/parameters/includeParameter',
+		},
+	],
+};
