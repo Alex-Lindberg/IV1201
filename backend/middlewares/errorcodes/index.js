cdb27b4354b6d22ac6b0f13d5b81ca7b754aa755
Dennis Hadzialic <25294013+D3nnis38@users.noreply.github.com>
2023-03-04 18:20:56 +0100

    add boom error

--- backend/middlewares/errorcodes/index.js
+++ backend/middlewares/errorcodes/index.js
@@ -1,43 +1,52 @@
 const Boom = require('@hapi/boom'),
-	_ = require('lodash');
+  _ = require('lodash');
 
 const boomErrorCodes = {
-	forbidden: function ({ req, message = 'Forbidden' }) {
-		return Boom.forbidden('', {
-			statusCode: 403,
-			error: 'Forbidden',
-			method: req.method,
-			path: req.url,
-			message: message,
-		});
-	},
-	serverError: function ({ req, message = 'Server error' }) {
-		return Boom.badImplementation('', {
-			statusCode: 500,
-			errorCode: 'Server error',
-			method: req.method,
-			path: req.url,
-			message: message,
-		});
-	},
-	notFound: function ({ req, message = 'Not Found' }) {
-		return Boom.notFound('', {
-			statusCode: 404,
-			errorCode: 'Not found',
-			method: req.method,
-			path: req.url,
-			message: message,
-		});
-	},
-	badRequest: function ({ req, message = 'Invalid value' }) {
-		return Boom.badRequest('', {
-			statusCode: 400,
-			errorCode: 'Invalid value',
-			method: req.method,
-			path: req.url,
-			message: message,
-		});
-	},
+  forbidden: function ({ req, message = 'Forbidden' }) {
+    return Boom.forbidden('', {
+      statusCode: 403,
+      error: 'Forbidden',
+      method: req.method,
+      path: req.url,
+      message: message,
+    });
+  },
+  serverError: function ({ req, message = 'Server error' }) {
+    return Boom.badImplementation('', {
+      statusCode: 500,
+      errorCode: 'Server error',
+      method: req.method,
+      path: req.url,
+      message: message,
+    });
+  },
+  notFound: function ({ req, message = 'Not Found' }) {
+    return Boom.notFound('', {
+      statusCode: 404,
+      errorCode: 'Not found',
+      method: req.method,
+      path: req.url,
+      message: message,
+    });
+  },
+  badRequest: function ({ req, message = 'Invalid value' }) {
+    return Boom.badRequest('', {
+      statusCode: 400,
+      errorCode: 'Invalid value',
+      method: req.method,
+      path: req.url,
+      message: message,
+    });
+  },
+  unauthorized: function ({ req, message = 'Unauthorized' }) {
+    return Boom.unauthorized('', 'sample', {
+      statusCode: 401,
+      errorCode: 'Unauthorized',
+      method: req.method,
+      path: req.url,
+      message: message,
+    });
+  },
 };
 
 /**
@@ -48,17 +57,17 @@ const boomErrorCodes = {
  * @param next - The next middleware in the chain.
  */
 const sendErrorCodes = (req, res, next) => {
-	res.status(200).json(
-		_(boomErrorCodes)
-			.values()
-			.map((x) => {
-				return x({ req }).data;
-			})
-			.value()
-	);
+  res.status(200).json(
+    _(boomErrorCodes)
+      .values()
+      .map((x) => {
+        return x({ req }).data;
+      })
+      .value()
+  );
 };
 
 module.exports = {
-	errorCodes: boomErrorCodes,
-	sendErrorCodes,
+  errorCodes: boomErrorCodes,
+  sendErrorCodes,
 };
