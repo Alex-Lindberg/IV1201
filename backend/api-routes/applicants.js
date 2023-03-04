fb593b16143a05c72b4c17846811864a08eb2ef3
Dennis Hadzialic <25294013+D3nnis38@users.noreply.github.com>
2023-03-04 20:08:32 +0100

    add personId and sessionId

--- backend/api-routes/applicants.js
+++ backend/api-routes/applicants.js
@@ -13,34 +13,40 @@ module.exports = {
 };
 
 module.exports.get.apiDoc = {
-	tags: ['applicants'],
-	responses: {
-		200: {
-			description: 'Successfully fetched list of applicants',
-			content: {
-				'application/json': {
-					schema: {
-						$ref: '#/components/schemas/SearchResults',
-					},
-				},
-			},
-		},
-	},
-	parameters: [
-		{
-			$ref: '#/components/parameters/size',
-		},
-		{
-			$ref: '#/components/parameters/offset',
-		},
-		{
-			$ref: '#/components/parameters/filterString',
-		},
-		{
-			$ref: '#/components/parameters/orderBy',
-		},
-		{
-			$ref: '#/components/parameters/filterBy',
-		},
-	],
+  tags: ['applicants'],
+  responses: {
+    200: {
+      description: 'Successfully fetched list of applicants',
+      content: {
+        'application/json': {
+          schema: {
+            $ref: '#/components/schemas/SearchResults',
+          },
+        },
+      },
+    },
+  },
+  parameters: [
+    {
+      $ref: '#/components/parameters/size',
+    },
+    {
+      $ref: '#/components/parameters/offset',
+    },
+    {
+      $ref: '#/components/parameters/filterString',
+    },
+    {
+      $ref: '#/components/parameters/orderBy',
+    },
+    {
+      $ref: '#/components/parameters/filterBy',
+    },
+    {
+      $ref: '#/components/parameters/personId',
+    },
+    {
+      $ref: '#/components/parameters/sessionId',
+    },
+  ],
 };
