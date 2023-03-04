3a077e69e17d1ef95364193778f882594353034b
Dennis Hadzialic <25294013+D3nnis38@users.noreply.github.com>
2023-03-04 18:20:31 +0100

    add request headers

--- backend/api-routes/applicants/{applicantId}.js
+++ backend/api-routes/applicants/{applicantId}.js
@@ -1,39 +1,45 @@
 const auth = require('../../middlewares/auth'),
-	applicantsMiddleware = require('../../middlewares/applicants'),
-	responseMiddleware = require('../../middlewares/response');
+  applicantsMiddleware = require('../../middlewares/applicants'),
+  responseMiddleware = require('../../middlewares/response');
 
 module.exports = {
-	get: [
-		auth.initLocals,
-		auth.authorize,
-		applicantsMiddleware.initLocals,
-		applicantsMiddleware.getApplicant,
-		applicantsMiddleware.getAvailabilityForApplicant,
-		applicantsMiddleware.getCompetenceForApplicant,
-		responseMiddleware.sendResponse(200, 'applicant'),
-	],
+  get: [
+    auth.initLocals,
+    auth.authorize,
+    applicantsMiddleware.initLocals,
+    applicantsMiddleware.getApplicant,
+    applicantsMiddleware.getAvailabilityForApplicant,
+    applicantsMiddleware.getCompetenceForApplicant,
+    responseMiddleware.sendResponse(200, 'applicant'),
+  ],
 };
 
 module.exports.get.apiDoc = {
-	tags: ['applicants'],
-	responses: {
-		200: {
-			description: 'Successfully fecthed an applicant',
-			content: {
-				'application/json': {
-					schema: {
-						$ref: '#/components/schemas/Applicant',
-					},
-				},
-			},
-		},
-	},
-	parameters: [
-		{
-			$ref: '#/components/parameters/includeParameter',
-		},
-		{
-			$ref: '#/components/parameters/applicantId',
-		},
-	],
+  tags: ['applicants'],
+  responses: {
+    200: {
+      description: 'Successfully fecthed an applicant',
+      content: {
+        'application/json': {
+          schema: {
+            $ref: '#/components/schemas/Applicant',
+          },
+        },
+      },
+    },
+  },
+  parameters: [
+    {
+      $ref: '#/components/parameters/includeParameter',
+    },
+    {
+      $ref: '#/components/parameters/applicantId',
+    },
+    {
+      $ref: '#/components/parameters/personId',
+    },
+    {
+      $ref: '#/components/parameters/sessionId',
+    },
+  ],
 };
