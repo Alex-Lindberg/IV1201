6a168e1f33537f81ba650f6556074f84858785a1
Lucas Larsson <70958503+lucas-larsson@users.noreply.github.com>
2023-03-07 19:49:23 +0100

    removed logs

--- backend/middlewares/applicants/applicantsDAO.js
+++ backend/middlewares/applicants/applicantsDAO.js
@@ -1,7 +1,4 @@
 const { sendQuery } = require('../../utils/dbIntegration/dbConfig');
-const {
-	applicantId,
-} = require('../../api-documentation/components/parameters');
 
 const roleMap = {
 	recruiter: 1,
@@ -10,10 +7,6 @@ const roleMap = {
 
 const getApplicants = async (filterString, orderBy, filterBy, offset, size) => {
 
-	// const countQuery = `SELECT COUNT(*) FROM person WHERE role_id = $1
-	// ${!!filterString ? `AND surname LIKE '%' || $2 || '%'` : ''}`;
-	console.log('filterString', filterString);
-	console.log('filterBy', filterBy);
 	const query =
 		!!filterString && !!filterBy ?
 		`SELECT person_id, name, surname, pnr, email, username
@@ -36,7 +29,6 @@ const getApplicants = async (filterString, orderBy, filterBy, offset, size) => {
 			!!filterString && !!filterBy
 				? [roleMap.applicant, filterString]
 				: [roleMap.applicant];
-		// const countResult = await sendQuery(countQuery, queryParameters);
 		const result = await sendQuery(query, queryParameters);
 		const applicants = !size
 			? result.rows
