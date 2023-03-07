19a2f6e96edfb7873a8bd10db959d6c27cbb7716
Dennis Hadzialic <25294013+D3nnis38@users.noreply.github.com>
2023-03-07 17:57:41 +0100

    create applications file

---
+++ backend/api-routes/applications.js
@@ -0,0 +1,52 @@
+const auth = require('../middlewares/auth'),
+  applicantsMiddleware = require('../middlewares/applicants'),
+  responseMiddleware = require('../middlewares/response');
+
+module.exports = {
+  get: [
+    auth.initLocals,
+    auth.authorize,
+    applicantsMiddleware.initLocals,
+    applicantsMiddleware.getAllApplicants,
+    responseMiddleware.sendResponse(200, 'applicants'),
+  ],
+};
+
+module.exports.get.apiDoc = {
+  tags: ['applicants'],
+  responses: {
+    200: {
+      description: 'Successfully fetched list of applicants',
+      content: {
+        'application/json': {
+          schema: {
+            $ref: '#/components/schemas/SearchResults',
+          },
+        },
+      },
+    },
+  },
+  parameters: [
+    {
+      $ref: '#/components/parameters/size',
+    },
+    {
+      $ref: '#/components/parameters/offset',
+    },
+    {
+      $ref: '#/components/parameters/filterString',
+    },
+    {
+      $ref: '#/components/parameters/orderBy',
+    },
+    {
+      $ref: '#/components/parameters/filterBy',
+    },
+    {
+      $ref: '#/components/parameters/personId',
+    },
+    {
+      $ref: '#/components/parameters/sessionId',
+    },
+  ],
+};
