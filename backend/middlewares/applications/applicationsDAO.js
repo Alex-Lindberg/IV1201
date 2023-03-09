6d7b65fa6a0e17155b01902cb5032bf15a056db5
Dennis Hadzialic <25294013+D3nnis38@users.noreply.github.com>
2023-03-09 21:54:48 +0100

    add finall dao-function

--- backend/middlewares/applications/applicationsDAO.js
+++ backend/middlewares/applications/applicationsDAO.js
@@ -1,36 +1,43 @@
 const { client } = require('../../utils/dbIntegration/dbConfig');
 
-// const
-
-const insertCompetenceProfile = async ({}, { years_of_experience, competence_id }, person_id) => {
-  await sendSerializedQuery('BEGIN');
-  const query = `INSERT INTO competence_profile (years_of_experience, competence_id, person_id) VALUES ($1, $2, $3) RETURNING competence_id, years_of_experience, competence_profile_id`;
+const insertApplication = async (person_id, availabilities, competences) => {
+  const availabilityQuery = `INSERT INTO availability (from_date, to_date, person_id) VALUES ($1, $2, $3) RETURNING from_date, to_date, availability_id`;
+  const competenceQuery = `INSERT INTO competence_profile (years_of_experience, competence_id, person_id) VALUES ($1, $2, $3) RETURNING competence_id, years_of_experience, competence_profile_id`;
   try {
-    const result = await sendSerializedQuery(query, [
-      years_of_experience,
-      competence_id,
-      person_id,
-    ]);
-    return result.rows[0];
+    await client.query('BEGIN');
+    const availabilityResults = await Promise.all(
+      availabilities.map(async (availability) => {
+        const result = await client.query(availabilityQuery, [
+          availability.from_date,
+          availability.to_date,
+          person_id,
+        ]);
+        return result.rows[0];
+      })
+    );
+    const competenceResults = await Promise.all(
+      competences.map(async (competence) => {
+        const result = await client.query(competenceQuery, [
+          competence.years_of_experience,
+          competence.competence_id,
+          person_id,
+        ]);
+        return result.rows[0];
+      })
+    );
+    await client.query('COMMIT');
+    return {
+      availabilities: availabilityResults,
+      competences: competenceResults,
+    };
   } catch (err) {
-    throw err;
-  }
-};
-
-const insertAvailability = async ({ from_date, to_date }, person_id) => {
-  const query = `INSERT INTO availability (from_date, to_date, person_id) VALUES ($1, $2, $3) RETURNING from_date, to_date, availability_id`;
-  try {
-    const result = await sendSerializedQuery(query, [from_date, to_date, person_id]);
-    await sendSerializedQuery('COMMIT');
-    return result.rows[0];
-  } catch (err) {
+    client.query('ROLLBACK');
     throw err;
   }
 };
 
 module.exports = {
   applicationsDAO: {
-    insertCompetenceProfile,
-    insertAvailability,
+    insertApplication,
   },
 };
