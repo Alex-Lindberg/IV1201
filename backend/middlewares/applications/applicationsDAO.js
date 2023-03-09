4395c6c0dc7b1318dba053908468e04b1550cf64
Dennis Hadzialic <25294013+D3nnis38@users.noreply.github.com>
2023-03-09 21:36:08 +0100

    fix error

--- backend/middlewares/applications/applicationsDAO.js
+++ backend/middlewares/applications/applicationsDAO.js
@@ -1,22 +1,16 @@
-const { sendCustomQuery } = require('../../utils/dbIntegration/dbConfig');
-const beginQuery = async () => {
-  try {
-    return await sendCustomQuery('BEGIN');
-  } catch (err) {
-    throw err;
-  }
-};
-const commitQuery = async () => {
-  try {
-    return await sendCustomQuery('COMMIT');
-  } catch (err) {
-    throw err;
-  }
-};
-const insertCompetenceProfile = async ({ years_of_experience, competence_id }, person_id) => {
+const { client } = require('../../utils/dbIntegration/dbConfig');
+
+// const
+
+const insertCompetenceProfile = async ({}, { years_of_experience, competence_id }, person_id) => {
+  await sendSerializedQuery('BEGIN');
   const query = `INSERT INTO competence_profile (years_of_experience, competence_id, person_id) VALUES ($1, $2, $3) RETURNING competence_id, years_of_experience, competence_profile_id`;
   try {
-    const result = await sendCustomQuery(query, [years_of_experience, competence_id, person_id]);
+    const result = await sendSerializedQuery(query, [
+      years_of_experience,
+      competence_id,
+      person_id,
+    ]);
     return result.rows[0];
   } catch (err) {
     throw err;
@@ -26,7 +20,8 @@ const insertCompetenceProfile = async ({ years_of_experience, competence_id }, p
 const insertAvailability = async ({ from_date, to_date }, person_id) => {
   const query = `INSERT INTO availability (from_date, to_date, person_id) VALUES ($1, $2, $3) RETURNING from_date, to_date, availability_id`;
   try {
-    const result = await sendCustomQuery(query, [from_date, to_date, person_id]);
+    const result = await sendSerializedQuery(query, [from_date, to_date, person_id]);
+    await sendSerializedQuery('COMMIT');
     return result.rows[0];
   } catch (err) {
     throw err;
@@ -37,7 +32,5 @@ module.exports = {
   applicationsDAO: {
     insertCompetenceProfile,
     insertAvailability,
-    beginQuery,
-    commitQuery,
   },
 };
