89ef129eed6cbeedfa5b578684c923eb9b11fa91
Dennis Hadzialic <25294013+D3nnis38@users.noreply.github.com>
2023-03-10 09:00:39 +0100

    document frontend api-calls

--- frontend/src/api/applications.js
+++ frontend/src/api/applications.js
@@ -9,6 +9,18 @@ const defaultParams = {
 	orderBy: 'asc',
 };
 
+/**
+ * It fetches a list of applicants from the backend API, and returns the data and the parameters used
+ * to fetch the data
+ * @param [offset=0] - The offset of the data to be fetched.
+ * @param [params] - {
+ * @returns An object with the following properties:
+ * 	- count: number of applicants
+ * 	- next: url to the next page of applicants
+ * 	- previous: url to the previous page of applicants
+ * 	- results: array of applicants
+ * 	- params: the params used to fetch the applicants
+ */
 export const fetchApplications = async (offset = 0, params = defaultParams) => {
 	return api
 		.get(`${API_URL}/api/applicants`, { params })
@@ -19,6 +31,10 @@ export const fetchApplications = async (offset = 0, params = defaultParams) => {
 		.catch(console.error);
 };
 
+/**
+ * It fetches an applicant from the API and returns the data
+ * @returns The data from the API call.
+ */
 export const fetchApplicant = async ({ queryKey }) => {
 	const [_key, { personId }] = queryKey;
 	if (!!personId) {
@@ -33,6 +49,11 @@ export const fetchApplicant = async ({ queryKey }) => {
 	}
 };
 
+/**
+ * It takes a submission object, sends it to the server, and returns the response
+ * @param submission - an object containing the following properties:
+ * @returns The data from the API call.
+ */
 export const submitApplication = async (submission) => {
 	return api
 		.post(`${API_URL}/api/applications`, submission)
